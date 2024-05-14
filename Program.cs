f07e3056e3b9c9c13df8d50d620bf1a8d1ea6858
Maria Victoria Gimenez <gimenezmariavictoria95@gmail.com>
2024-05-14 12:26:27 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int invertir = 0;
+
+
+Console.WriteLine("Ingrese un numero");
+string texto = Console.ReadLine();
+
+if(int.TryParse(texto, out a))
+{
+   if(a > 0)
+   {
+    while(a != 0)
+    {
+        invertir = invertir * 10 + a % 10;
+        a = a / 10;
+    }
+   }
+}
+ Console.WriteLine(invertir);
+
+
+
+
