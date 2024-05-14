f9268d3c9b9553177592247a842e0dfed7ace359
Mariano <marianogotardo@gmail.com>
2024-05-14 13:16:56 -0300

    operaciones basicas

--- Program.cs
+++ Program.cs
@@ -23,6 +23,30 @@ if(int.TryParse(texto, out a))
 
 // Punto 2 //
 
+Console.WriteLine("1:Sumar dos numeros");
+Console.WriteLine("2:Restar dos numeros");
+Console.WriteLine("3:Multiplicar dos numeros");
+Console.WriteLine("4:Dividir dos numeros");
+string texto2 = Console.ReadLine();
+int opcion, numero1, numero2, resultado;
 
+if(int.TryParse(texto2, out opcion))
+{
+    switch(opcion)
+    {
+        case 1:
+        Console.WriteLine("Ingrese el numero 1");
+        string num1 = Console.ReadLine();
+        Console.WriteLine("Ingrese el numero 2");
+        string num2 = Console.ReadLine();
+        if(int.TryParse(num1, out numero1) && int.TryParse(num2, out numero2))
+        {
+            resultado = numero1 + numero2;
+            Console.WriteLine("El resultado de la suma es: " + resultado);
+        }
+        break;
+
+    }
+}
 
 
