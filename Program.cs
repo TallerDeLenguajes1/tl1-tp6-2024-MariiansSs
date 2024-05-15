5a9f83a9cdaaceec406358033dcd4ec1c2920177
Mariano <marianogotardo@gmail.com>
2024-05-15 11:31:06 -0300

    Agregue hasta subcadena

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 // Punto 1 //
+/*
 int a;
 int invertir = 0;
 
@@ -23,7 +24,21 @@ if(int.TryParse(texto, out a))
    }
 }
  Console.WriteLine(invertir);
+*/
+//Punto 4//
 
+Console.WriteLine("Ingrese texto");
+string cadena = Console.ReadLine();
 
+int longitud = cadena.Length;
+
+Console.WriteLine("La longitud de la cadena es:"+ cadena.Length);
+
+//Console.Write("Ingrese otro texto");
+//string cadena2 = Console.ReadLine();
+
+//Console.Write($"Lo ingresado fue: {cadena} {cadena2}");
+
+Console.Write(cadena.Substring(0,2));
 
 
